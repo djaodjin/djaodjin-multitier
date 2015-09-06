176efc439d2e1dabbf313bebb958d963a40ede32
Sebastien Mirolo <smirolo@djaodjin.com>
2015-09-05 18:37:59 -0700

    use template nodes as done in Django itself

--- multitier/templatetags/multitier_static.py
+++ multitier/templatetags/multitier_static.py
@@ -22,17 +22,42 @@
 # OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-
 from django import template
-from django.templatetags.static import static as django_static
+from django.utils.encoding import iri_to_uri
+from django.templatetags.static import StaticNode
+
+from ..locals import get_current_site
+
 
 register = template.Library()
 
-@register.simple_tag(takes_context=True)
-def static(context, src):
-    site = context['site']
-    prefix_src = site.theme + "/static"
-    if src.startswith("/"):
-        return django_static(prefix_src + src)
-    else:
-        return django_static(prefix_src + "/" + src)
+
+class MultitierStaticNode(StaticNode):
+
+    def url(self, context):
+        path = super(MultitierStaticNode, self).url(context)
+        site = get_current_site()
+        if site.theme:
+            return '/%s%s' % (iri_to_uri(site.theme), path)
+        return path
+
+
+@register.tag('static')
+def do_static(parser, token):
+    """
+    A template tag that returns the URL to a file
+    using a multitier ``Site.theme``
+
+    Usage::
+
+        {% static path [as varname] %}
+
+    Examples::
+
+        {% static "myapp/css/base.css" %}
+        {% static variable_with_path %}
+        {% static "myapp/css/base.css" as admin_base_css %}
+        {% static variable_with_path as varname %}
+
+    """
+    return MultitierStaticNode.handle_token(parser, token)
