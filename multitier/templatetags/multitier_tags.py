f09846cc94cbbdedc8ea0a8f47229be4c8e6f97e
Sebastien Mirolo <smirolo@djaodjin.com>
2016-03-25 14:30:08 -0700

    normalize filenames

--- multitier/templatetags/multitier_tags.py
+++ multitier/templatetags/multitier_tags.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015, DjaoDjin inc.
+# Copyright (c) 2016, DjaoDjin inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -23,9 +23,14 @@
 # ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 from django import template
+from django.contrib.messages.api import get_messages
+from django.forms import Form
 from django.templatetags.static import StaticNode
 
 from ..locals import get_current_site
+
+#pylint:disable=no-name-in-module,import-error
+from django.utils.six.moves.urllib.parse import urljoin
 
 
 register = template.Library()
@@ -35,10 +40,7 @@ class MultitierStaticNode(StaticNode):
 
     def url(self, context):
         path = super(MultitierStaticNode, self).url(context)
-        site = get_current_site()
-        if site.path_prefix:
-            return '/%s%s' % (site.subdomain, path)
-        return path
+        return site_prefixed(path)
 
 
 @register.tag('static')
@@ -60,3 +62,35 @@ def do_static(parser, token):
 
     """
     return MultitierStaticNode.handle_token(parser, token)
+
+
+@register.filter()
+def asset(path):
+    """
+    Adds the appropriate url or path prefix.
+
+    While ``{% static path [as varname] %}`` would work in the context
+    of Django templates, ``{{ path|asset }}`` works in both Django and Jinja2
+    templates.
+    """
+    return site_prefixed(path)
+
+
+@register.filter()
+def messages(obj):
+    """
+    Messages to be displayed to the current session.
+    """
+    if isinstance(obj, Form):
+        return obj.non_field_errors()
+    return get_messages(obj)
+
+
+@register.filter()
+def site_prefixed(path):
+    site = get_current_site()
+    if site.path_prefix:
+        path_prefix = '/%s/' % site.path_prefix
+    else:
+        path_prefix = '/'
+    return urljoin(path_prefix, path)
