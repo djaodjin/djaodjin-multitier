575e32bd83b59432257476112e08f37c2a1a0b72
Sebastien Mirolo <smirolo@djaodjin.com>
2021-07-30 16:20:28 -0700

    releases 0.1.20 to pypi

--- multitier/__init__.py
+++ multitier/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2020, Djaodjin Inc.
+# Copyright (c) 2021, Djaodjin Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -26,4 +26,4 @@
 PEP 386-compliant version number for the multitier django app.
 """
 
-__version__ = '0.1.19'
+__version__ = '0.1.20'
